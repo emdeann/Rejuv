156151b9dc80af986e5b387f2b787d030d9397a2
Dean <me@deann.dev>
2024-08-16 20:36:49 -0700

    Add drive backup

--- sync_saves.py
+++ sync_saves.py
@@ -6,6 +6,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive"]
@@ -18,50 +19,45 @@ def save_backup():
     # Copy current save to backup folder
     shutil.copy2('Game.rxdata', 'Backup')
 
+def send_backup_to_drive(service):
+    backup_folder_id = '1qshwEt0oau4aMw0-VLxCR8lpiUexEhV4'
+    metadata = {'name': 'Game.rxdata', 'parents': [backup_folder_id]}
+    media = MediaFileUpload(filename='Game.rxdata', mimetype='application/octet-stream')
+    file = service.files().create(body=metadata, media_body=media, fields='id').execute()
+    return file.get('id')
+
+def api_login():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    try:
+        service = build("drive", "v3", credentials=creds)  
+    except HttpError as error:
+        print(error)
+        
+    return service
+
 def main():
-  """Shows basic usage of the Drive v3 API.
-  Prints the names and ids of the first 10 files the user has access to.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+    save_backup()
+    service = api_login()
+    send_backup_to_drive(service)
 
-  try:
-    service = build("drive", "v3", credentials=creds)
-
-    # Call the Drive v3 API
-    results = (
-        service.files()
-        .list(pageSize=10, fields="nextPageToken, files(id, name)")
-        .execute()
-    )
-    items = results.get("files", [])
-
-    if not items:
-      print("No files found.")
-      return
-    print("Files:")
-    for item in items:
-      print(f"{item['name']} ({item['id']})")
-  except HttpError as error:
-    # TODO(developer) - Handle errors from drive API.
-    print(f"An error occurred: {error}")
 
 
 if __name__ == "__main__":
-  save_backup()
+  main()
