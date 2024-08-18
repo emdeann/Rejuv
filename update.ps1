7d2ad5bbf2e41c0f181231675e44ea7ebf5fbda2
Dean <me@deann.dev>
2024-08-18 14:56:53 -0700

    Add powershell script to update when new save is made

---
+++ update.ps1
@@ -0,0 +1,34 @@
+﻿Function UploadSave {
+    # Assumes a correct pipenv environment
+    pipenv run python upload_save.py
+
+}
+
+Function Watch {    
+    $global:FileChanged = $false
+    $folder = "C:/Users/Dean/Saved Games/Rejuv"
+    $filter = "Game.rxdata"
+    $watcher = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ 
+        IncludeSubdirectories = $false 
+        EnableRaisingEvents = $true
+    }
+
+    Register-ObjectEvent $Watcher -EventName Changed -Action {$global:FileChanged = $true} > $null
+
+    while ($true){
+        while ($global:FileChanged -eq $false){
+            # We need this to block the IO thread until there is something to run 
+            # so the script doesn't finish. If we call the action directly from 
+            # the event it won't be able to write to the console
+            Start-Sleep -Milliseconds 100
+        }
+
+        # a file has changed, run our stuff on the I/O thread so we can see the output
+        UploadSave
+
+        # reset and go again
+        $global:FileChanged = $false
+    }
+}
+
+Watch
